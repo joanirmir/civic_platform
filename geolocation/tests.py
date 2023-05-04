4657353aab198d556db7db9a00a0c0c55ba5ea92
joanirmir <joanirmir@gmail.com>
2023-05-04 15:33:58 +0200

    Passing test for geolocation model

--- geolocation/tests.py
+++ geolocation/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from unittest.mock import patch
+from .models import Location
 
-# Create your tests here.
+
+class LocationTestCase(TestCase):
+    def test_get_coordinates_from_city(self):
+        with patch.object(
+            Location, "get_coordinates_from_city", return_value=(52.5200066, 13.404954)
+        ):
+            latitude, longitude = Location.get_coordinates_from_city("Berlin")
+            self.assertEqual(latitude, 52.5200066)
+            self.assertEqual(longitude, 13.404954)
+
+        with patch.object(
+            Location, "get_coordinates_from_city", return_value=(None, None)
+        ):
+            latitude, longitude = Location.get_coordinates_from_city("Invalid City")
+            self.assertEqual(latitude, None)
+            self.assertEqual(latitude, None)
