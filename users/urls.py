e4d4619626bfc9054b9191d5c8bf1e95db608909
joanirmir <joanirmir@gmail.com>
2023-05-12 13:42:23 +0200

    Modified url pattern in user app

--- users/urls.py
+++ users/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path("profile/<int:pk>", views.UserApiView.as_view(), name="logged-in-profile"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    #path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
